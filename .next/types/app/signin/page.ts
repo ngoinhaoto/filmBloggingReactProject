db08ced35eb84d8ac9773129d46d290dceb88805
ngoinhaoto <dinhchinhle487@gmail.com>
2023-12-05 18:03:22 +0700

    added signin

--- .next/types/app/signin/page.ts
+++ .next/types/app/signin/page.ts
@@ -1,8 +1,8 @@
-// File: /Users/ritherthemuncher/Desktop/funny ai /filmBloggingReactProject/src/app/page.js
-import * as entry from '../../../src/app/page.js'
+// File: /Users/ritherthemuncher/Desktop/funny ai /filmBloggingReactProject/src/app/signin/page.js
+import * as entry from '../../../../src/app/signin/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
-type TEntry = typeof import('../../../src/app/page.js')
+type TEntry = typeof import('../../../../src/app/signin/page.js')
 
 // Check that the entry is a valid entry
 checkFields<Diff<{
---
+++ src/app/signin/page.js
@@ -0,0 +1,76 @@
+"use client";
+
+import React, { useState } from "react";
+import styles from "../css/signin.css";
+
+const SignIn = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(formData);
+  };
+
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-purple-100">
+      <h1 className="text-3xl lg:text-4xl font-bold mb-5 text-center text-purple-800">
+        Sign In
+      </h1>
+
+      <div className="bg-white p-8 mx-4 rounded-2xl max-w-md w-full lg:max-w-3xl md:max-w-2xl border-2 border-gray-600">
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label htmlFor="username" className="block text-gray-700 mb-2">
+              Username
+            </label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
+              className="border rounded px-4 py-2 w-full focus:outline-none focus:border-purple-800 "
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="password" className="block text-gray-700 mb-2">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              className="border rounded px-4 py-2 w-full focus:outline-none focus:border-purple-800"
+              required
+            />
+          </div>
+          <div className="mt-6">
+            <button
+              type="submit"
+              className="bg-purple-800 text-white px-4 py-2 rounded hover:bg-purple-600 focus:outline-none w-full transition duration-500 ease-in-out"
+            >
+              Sign In
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;

914be208494967498fe2556d6e1131f351ba0e56
Trưởng Phạm <truong.pham210214@vnuk.edu.vn>
2023-12-08 15:22:04 +0700

    add nextui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-const { addDynamicIconSelectors } = require("@iconify/tailwind");
+/** * @type {import('tailwindcss').Config} */
+import {nextui} from "@nextui-org/react";
 
 module.exports = {
   content: [
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -17,5 +18,6 @@ module.exports = {
       },
     },
   },
-  plugins: [addDynamicIconSelectors()],
+  darkMode: "class",
+  plugins: [nextui()],
 };

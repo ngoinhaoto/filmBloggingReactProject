822b97fa945324d80a20dfa98fd6a676affcf117
ngoinhaoto <dinhchinhle487@gmail.com>
2024-01-09 17:35:52 +0700

    adding sign up

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -1,5 +1,51 @@
 import prisma from "../../../../lib/prisma";
+import { NextResponse, NextRequest } from "next/server";
 
-export default async function POST(request, res) {
-    
+export async function POST(req) {
+  if (req.method !== "POST") {
+    return NextResponse.json(
+      { message: "Method Not Allowed" },
+      { status: 200 }
+    );
+  }
+
+  const result = await req.json();
+  console.log("RESULT: ", result);
+  const { username, password, displayName, location } = result;
+
+  const avatar = "https://i.imgur.com/UEO3Ul7.jpeg";
+
+  try {
+    // Check if the username already exists in the database
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        username,
+      },
+    });
+
+    if (existingUser) {
+      return NextResponse.json(
+        { message: "Username is already taken" },
+        { status: 400 }
+      );
+    }
+
+    const newAccount = await prisma.user.create({
+      data: {
+        username,
+        password,
+        displayName,
+        location,
+        avatar,
+      },
+    });
+
+    return Response.json({ newAccount }, { status: 201 });
+  } catch (error) {
+    console.error("Error creating account:", error);
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
 }

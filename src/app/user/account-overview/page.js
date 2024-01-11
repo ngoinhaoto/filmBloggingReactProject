67f3d48327ceca40b5e3d0feae68bc29e3a541f5
ngoinhaoto <dinhchinhle487@gmail.com>
2024-01-11 11:45:05 +0700

    changing account overview to client side rendering and add skeleton loading

--- src/app/user/account-overview/page.js
+++ src/app/user/account-overview/page.js
@@ -1,27 +1,41 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import NavBarHomePage from "../../../components/navbar/NavBarHomePage";
 import UserSideBar from "../../user/userSideBar/userSideBar";
-
 import Footer from "../../../components/footer/Footer";
-
-import { getServerSession } from "next-auth";
 import { useSession } from "next-auth/react";
-import { authOptions } from "../../api/auth/[...nextauth]/route";
-import Image from "next/image";
-export default async function AccountOverview() {
-  const session = await getServerSession(authOptions);
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
-  const userID = session.user.id;
+export default function AccountOverview() {
+  const { data: session, status } = useSession();
+  const [userData, setUserData] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-  const data = await getServerSideProps(userID);
-  // console.log(data.userOverview.post.length);
-  // console.log(data.userOverview.comment.length);
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        if (status === "authenticated" && session?.user?.id) {
+          const response = await fetch(`/api/user/${session.user.id}`);
+          if (response.ok) {
+            const fetchedUserData = await response.json();
+            setUserData(fetchedUserData);
+          }
+        }
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      } finally {
+        setLoading(false); // Set loading to false regardless of success or failure
+      }
+    };
+
+    fetchUserData();
+  }, [session, status]);
 
   return (
     <>
       <div className="bg-white">
         <NavBarHomePage />
-
         <div className="container mx-auto p-4 md:p-0">
           <div className="md:flex">
             <UserSideBar />
@@ -30,55 +44,63 @@ export default async function AccountOverview() {
 
               <div className="user-overview flex-col flex items-center align-middle justify-center">
                 <div className="bg-gray-200 h-32 w-32 text-center rounded-full overflow-hidden">
-                  <img
-                    src={data.userOverview.avatar}
-                    width={300}
-                    height={300}
-                    alt="Avatar"
-                    className="h-full w-full object-cover"
-                  />
+                  {loading ? (
+                    <Skeleton height={300} width={300} />
+                  ) : (
+                    <img
+                      src={userData?.userOverview?.avatar}
+                      width={300}
+                      height={300}
+                      alt="Avatar"
+                      className="h-full w-full object-cover"
+                    />
+                  )}
                 </div>
                 <p className="mt-2 mb-4 font-bold text-center">
-                  {data.userOverview.displayName}
+                  {loading ? (
+                    <Skeleton width={150} />
+                  ) : (
+                    userData?.userOverview?.displayName
+                  )}
                 </p>
                 <div className="container flex mb-4 items-start justify-between">
-                  <div className="w-1/2">User ID: {data.userOverview.id}</div>
                   <div className="w-1/2">
-                    Location:{data.userOverview.location}{" "}
+                    {loading ? (
+                      <Skeleton width={150} />
+                    ) : (
+                      `User ID: ${userData?.userOverview?.id}`
+                    )}
+                  </div>
+                  <div className="w-1/2">
+                    {loading ? (
+                      <Skeleton width={150} />
+                    ) : (
+                      `Location: ${userData?.userOverview?.location || "N/A"}`
+                    )}
                   </div>
                 </div>{" "}
                 <div className="container flex items-start justify-between">
                   <div className="w-1/2">
-                    Post number: {data.userOverview.post.length}
+                    {loading ? (
+                      <Skeleton width={150} />
+                    ) : (
+                      `Post number: ${userData?.userOverview?.post.length}`
+                    )}
                   </div>
                   <div className="w-1/2">
-                    Comment number: {data.userOverview.comment.length}
+                    {loading ? (
+                      <Skeleton width={150} />
+                    ) : (
+                      `Comment number: ${userData?.userOverview?.comment.length}`
+                    )}
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-
         <Footer />
       </div>
     </>
   );
 }
-async function getServerSideProps(userID) {
-  const res = await fetch(checkEnvironment().concat(`/api/user/${userID}`));
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
-
-  return res.json();
-}
-
-export const checkEnvironment = () => {
-  let base_url =
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:3000"
-      : "https://example.com"; // https://v2ds.netlify.app
-
-  return base_url;
-};

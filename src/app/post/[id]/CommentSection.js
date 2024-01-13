b9f6fa8a7315e993e4f5b380c02d7175c82aec28
ngoinhaoto <dinhchinhle487@gmail.com>
2024-01-13 10:55:52 +0700

    if theres session, give access to comment

--- src/app/post/[id]/CommentSection.js
+++ src/app/post/[id]/CommentSection.js
@@ -6,7 +6,7 @@ import { Button } from "@nextui-org/react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
-const CommentSection = ({ comments, postID, callback}) => {
+const CommentSection = ({ comments, postID, callback }) => {
   const { data: session, status } = useSession();
 
   const userID = session?.user?.id;
@@ -39,7 +39,6 @@ const CommentSection = ({ comments, postID, callback}) => {
   };
 
   const postComment = async () => {
-
     if (isFormValid) {
       setIsLoading(true);
       const currentDate = new Date().toISOString();
@@ -60,7 +59,7 @@ const CommentSection = ({ comments, postID, callback}) => {
       });
 
       if (response.ok) {
-        setNewComment("")
+        setNewComment("");
         callback();
         setIsLoading(false);
       }
@@ -70,27 +69,29 @@ const CommentSection = ({ comments, postID, callback}) => {
   return (
     <div className="container mx-auto p-4">
       <h2 className="text-3xl font-bold mb-4">Comments</h2>
-      <div className="flex flex-wrap md:flex-nowrap gap-4 items-center mb-4">
-        <Textarea
-          type="text"
-          color="secondary"
-          value={newComment}
-          onChange={(e) => setNewComment(e.target.value)}
-          label="Comment"
-          placeholder="Sending thoughts and prayers"
-          className="w-full md:w-8/12"
-          isInvalid={!!errors.comment}
-          errorMessage={errors.comment}
-        />
-        <Button
-          onClick={postComment}
-          radius="full"
-          className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg md:ml-2 w-full md:w-auto"
-          isLoading={isLoading ? true : false}
-        >
-          Post
-        </Button>
-      </div>
+      {session && (
+        <div className="flex flex-wrap md:flex-nowrap gap-4 items-center mb-4">
+          <Textarea
+            type="text"
+            color="secondary"
+            value={newComment}
+            onChange={(e) => setNewComment(e.target.value)}
+            label="Comment"
+            placeholder="Sending thoughts and prayers"
+            className="w-full md:w-8/12"
+            isInvalid={!!errors.comment}
+            errorMessage={errors.comment}
+          />
+          <Button
+            onClick={postComment}
+            radius="full"
+            className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg md:ml-2 w-full md:w-auto"
+            isLoading={isLoading ? true : false}
+          >
+            Post
+          </Button>
+        </div>
+      )}
       <div className="comments-list">
         {commentList.map((comment, index) => (
           <Comment key={index} comment={comment} />

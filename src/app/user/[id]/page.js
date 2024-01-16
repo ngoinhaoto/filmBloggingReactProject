cfa4cf8eb1865d6ba3d45736361c0efdd79ef2ad
ngoinhaoto <dinhchinhle487@gmail.com>
2024-01-16 20:38:59 +0700

    Update page.js

--- src/app/user/[id]/page.js
+++ src/app/user/[id]/page.js
@@ -7,14 +7,19 @@ import Skeleton from "react-loading-skeleton";
 export default function UserPage({ params }) {
   const [userOverview, setUserOverview] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [userNotFound, setUserNotFound] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch(`/api/user/${params.id}`);
-        const data = await response.json();
+        if (!response.ok) {
+          // If the response is not okay, set userNotFound to true
+          setUserNotFound(true);
+          return;
+        }
 
-        console.log(data);
+        const data = await response.json();
         setUserOverview(data.userOverview);
       } catch (error) {
         console.error("Error fetching user profile:", error);
@@ -30,7 +35,20 @@ export default function UserPage({ params }) {
     <>
       <div className="bg-white">
         <NavBarHomePage />
-        <div className="container mx-auto p-4"></div>
+        <div className="container mx-auto p-4">
+          {loading && <Skeleton height={200} count={5} />}
+
+          {!loading && userNotFound && (
+            <div className="text-xl md:text-4xl text-center font-bold text-red-500">
+              User not found!
+            </div>
+          )}
+
+          {!loading && !userNotFound && (
+            // Render user profile details here
+            <div></div>
+          )}
+        </div>
         <Footer />
       </div>
     </>

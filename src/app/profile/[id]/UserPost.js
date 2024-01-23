dfed608a2c780e513f3bc2894a585b5ededd6271
ngoinhaoto <dinhchinhle487@gmail.com>
2024-01-23 10:52:38 +0700

    added draft of profile

---
+++ src/app/profile/[id]/UserPost.js
@@ -0,0 +1,125 @@
+import React from "react";
+import { Chip } from "@nextui-org/react";
+import { Icon } from "@iconify/react";
+
+import timeLine from "@iconify/icons-mingcute/time-line";
+import Link from "next/link";
+
+const truncateContent = (content, wordCount) => {
+  const plainText = content.replace(/<[^>]*>/g, "");
+  const words = plainText.split(" ");
+  if (words.length > wordCount) {
+    return words.slice(0, wordCount).join(" ") + " ...";
+  }
+  return plainText;
+};
+
+const calculateDaysAgo = (timestamp) => {
+  const millisecondsPerMinute = 60 * 1000;
+  const millisecondsPerHour = 60 * millisecondsPerMinute;
+  const currentDate = new Date();
+  const postDate = new Date(timestamp);
+
+  const differenceInMilliseconds = currentDate - postDate;
+
+  if (differenceInMilliseconds < millisecondsPerHour) {
+    const minutesAgo = Math.floor(
+      differenceInMilliseconds / millisecondsPerMinute
+    );
+    return `${minutesAgo === 1 ? "1 minute ago" : `${minutesAgo} minutes ago`}`;
+  } else if (differenceInMilliseconds < 24 * millisecondsPerHour) {
+    const hoursAgo = Math.floor(differenceInMilliseconds / millisecondsPerHour);
+    return `${hoursAgo === 1 ? "1 hour ago" : `${hoursAgo} hours ago`}`;
+  } else {
+    const daysAgo = Math.floor(
+      differenceInMilliseconds / (24 * millisecondsPerHour)
+    );
+    return `${daysAgo === 1 ? "1 day ago" : `${daysAgo} days ago`}`;
+  }
+};
+
+const getCategoryChipStyles = (category) => {
+  switch (category.toLowerCase()) {
+    case "fantasy":
+      return {
+        base: "bg-gradient-to-br from-red-500 to-pink-500 border-small border-white/50 shadow-pink-500/30",
+        content: "drop-shadow shadow-black text-white",
+      };
+    case "horror":
+      return {
+        base: "bg-gradient-to-br from-green-500 to-yellow-500 border-small border-white/50 shadow-yellow-500/30",
+        content: "drop-shadow shadow-black text-white",
+      };
+    case "action":
+      return {
+        base: "bg-gradient-to-br from-blue-500 to-indigo-500 border-small border-white/50 shadow-indigo-500/30",
+        content: "drop-shadow shadow-black text-white",
+      };
+    case "romance":
+      return {
+        base: "bg-gradient-to-br from-purple-500 to-red-500 border-small border-white/50 shadow-red-500/30",
+        content: "drop-shadow shadow-black text-white",
+      };
+    default:
+      return {
+        base: "bg-gradient-to-br from-gray-500 to-gray-700 border-small border-white/50 shadow-gray-700/30",
+        content: "drop-shadow shadow-black text-white",
+      };
+  }
+};
+
+export default function UserPost({ post }) {
+  const truncatedContent = truncateContent(post.content, 10);
+  const daysAgo = calculateDaysAgo(post.createdAt);
+  const categoryChipStyles = getCategoryChipStyles(post.categories[0]);
+
+  return (
+    <Link href={`/post/${post.id}`} passHref>
+      <div className="bg-white flex shadow-lg rounded-xl flex-col md:flex-row my-2">
+        <div className="md:basis-1/4 md:h-auto relative">
+          <div
+            className="md:w-full h-60 md:h-full md:rounded-l-xl md:rounded-r-none rounded-t-lg bg-cover bg-center"
+            style={{ backgroundImage: `url(${post.thumbnail})` }}
+          ></div>
+        </div>
+        <div className="md:basis-3/4 flex-col p-6 w-full">
+          <div className="mb-6">
+            <div className="text-xl font-medium">{post.title}</div>
+            <div
+              className="text-gray-400"
+              dangerouslySetInnerHTML={{ __html: truncatedContent }}
+            ></div>
+          </div>
+          <div className="flex gap-1 flex-wrap mb-3">
+            {post.spoiledContent && (
+              <Chip color="secondary" variant="solid" radius="sm">
+                Spoiled
+              </Chip>
+            )}
+            {post.nsfw && (
+              <Chip color="danger" variant="solid" radius="sm">
+                NSFW
+              </Chip>
+            )}
+            <Chip
+              key={post.categories[0]}
+              classNames={categoryChipStyles}
+              variant="solid"
+              radius="sm"
+            >
+              {post.categories[0].charAt(0).toUpperCase() +
+                post.categories[0].slice(1)}{" "}
+            </Chip>
+          </div>
+
+          <div className="w-full flex">
+            <div className="flex items-center">
+              <Icon icon={timeLine} color="#4b5563" />
+              <p className="text-gray-600">{daysAgo}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+}
--- src/app/profile/[id]/page.js
+++ src/app/profile/[id]/page.js
@@ -4,10 +4,34 @@ import NavBarHomePage from "../../../components/navbar/NavBarHomePage";
 import Footer from "../../../components/footer/Footer";
 import Skeleton from "react-loading-skeleton";
 
+import Link from "next/link";
+import UserPost from "./UserPost";
+import { Avatar } from "@nextui-org/react";
+
+function formatDate(timestamp) {
+  const currentDate = new Date();
+  const commentDate = new Date(timestamp);
+  const timeDifference = currentDate - commentDate;
+  const seconds = Math.floor(timeDifference / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+  const months = Math.floor(days / 30);
+  const years = Math.floor(months / 12);
+
+  if (years > 0) return `${years} year${years > 1 ? "s" : ""} ago`;
+  if (months > 0) return `${months} month${months > 1 ? "s" : ""} ago`;
+  if (days > 0) return `${days} day${days > 1 ? "s" : ""} ago`;
+  if (hours > 0) return `${hours} hour${hours > 1 ? "s" : ""} ago`;
+  if (minutes > 0) return `${minutes} minute${minutes > 1 ? "s" : ""} ago`;
+  if (seconds > 0) return `${seconds} second${seconds > 1 ? "s" : ""} ago`;
+  return "Just now";
+}
+
 export default function UserPage({ params }) {
-  const [userOverview, setUserOverview] = useState(null);
   const [loading, setLoading] = useState(true);
   const [userNotFound, setUserNotFound] = useState(false);
+  const [userData, setUserData] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -22,7 +46,8 @@ export default function UserPage({ params }) {
 
         const data = await response.json();
         console.log(data);
-        setUserOverview(data.userOverview);
+
+        setUserData(data.userOverview);
       } catch (error) {
         console.error("Error fetching user profile:", error);
       } finally {
@@ -37,7 +62,7 @@ export default function UserPage({ params }) {
     <>
       <div className="bg-white">
         <NavBarHomePage />
-        <div className="container mx-auto p-4">
+        <div className="container mx-auto p-4 flex justify-center items-center">
           {loading && <Skeleton height={200} count={5} />}
 
           {!loading && userNotFound && (
@@ -47,8 +72,46 @@ export default function UserPage({ params }) {
           )}
 
           {!loading && !userNotFound && (
-            // Render user profile details here
-            <div></div>
+            <div className="flex-col">
+              <div className="flex-col text-center items-center">
+                <div className="w-36 h-36 mx-auto mb-4">
+                  <Avatar
+                    isBordered
+                    src={userData && userData.avatar}
+                    className="w-full h-full object-cover rounded-full"
+                  />
+                </div>
+                <div>Display Name: {userData.displayName}</div>
+                <div>Location: {userData.location}</div>
+                <div>Created: {formatDate(userData.createdAt)}</div>{" "}
+              </div>
+              <div className="mt-8">
+                <h2 className="text-2xl font-bold mb-4">User Posts:</h2>
+                <ul>
+                  {userData &&
+                    userData.post &&
+                    userData.post.map((post) => (
+                      <li key={post.id}>
+                        <UserPost post={post} />
+                      </li>
+                    ))}
+                </ul>
+              </div>
+              <div className="mt-8">
+                <h2 className="text-2xl font-bold mb-4">User Comments:</h2>
+                <ul>
+                  {userData &&
+                    userData.comment &&
+                    userData.comment.map((comment) => (
+                      <li key={comment.id}>
+                        <Link href={`/post/${comment.postId}`}>
+                          {comment.commentBody}
+                        </Link>
+                      </li>
+                    ))}
+                </ul>
+              </div>
+            </div>
           )}
         </div>
         <Footer />
